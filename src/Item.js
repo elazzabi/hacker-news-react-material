7b01ce91185018b9a3170fe1ec9bb9a7a08304dc
Ahmed <elazzabi.ahmed@gmail.com>
2017-01-31 20:37:45 +0000

    Fix comment vs comments text
    (and remove unused var)

--- src/Item.js
+++ src/Item.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import {Card, CardHeader, CardActions} from 'material-ui/Card'
 //import Avatar from 'material-ui/Avatar'
 //import { pink400 } from 'material-ui/styles/colors'
-import FlatButton from 'material-ui/FlatButton'
 import IconButton from 'material-ui/IconButton'
 import Link from 'material-ui/svg-icons/action/open-in-new'
 import Comment from 'material-ui/svg-icons/communication/comment'
@@ -22,7 +21,7 @@ class Item extends Component {
         style={{display: "flex", alignItems:"center"}}
       />
       <CardActions expandable={true} style={{float: "right", display: "flex", alignItems: "center"}}>
-        <IconButton tooltip={`${kids.length} comment`}>
+        <IconButton tooltip={kids.length + (kids.length <= 1 ? ' comment' : ' comments')}>
           <Comment />
         </IconButton>
         <IconButton href={url} tooltip="Open link">
