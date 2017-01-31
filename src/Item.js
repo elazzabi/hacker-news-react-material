bbb84722305582f7bf50e6ac87c06aa1b8ada77a
Ahmed <elazzabi.ahmed@gmail.com>
2017-01-31 20:25:02 +0000

    Implement open link and comments number
    (disable Avatar due to errors in stretching)

--- src/Item.js
+++ src/Item.js
@@ -1,23 +1,35 @@
-import React, { Component } from 'react';
-import {Card, CardHeader} from 'material-ui/Card';
-import Avatar from 'material-ui/Avatar';
-import { pink400 } from 'material-ui/styles/colors';
-
+import React, { Component } from 'react'
+import {Card, CardHeader, CardActions} from 'material-ui/Card'
+//import Avatar from 'material-ui/Avatar'
+//import { pink400 } from 'material-ui/styles/colors'
+import FlatButton from 'material-ui/FlatButton'
+import IconButton from 'material-ui/IconButton'
+import Link from 'material-ui/svg-icons/action/open-in-new'
+import Comment from 'material-ui/svg-icons/communication/comment'
 
 class Item extends Component {
   render() {
-    let {by, title, score} = this.props.data
+    let {by, title, score, url, kids} = this.props.data
 
     return <Card style={{marginTop: "16px"}}>
       <CardHeader
-        avatar={<Avatar backgroundColor={pink400}>{title[0]}</Avatar>}
+        // Commented due to stretching problems in Avatar
+        //avatar={<Avatar backgroundColor={pink400}>{title[0]}</Avatar>}
         title={title}
         subtitle={`by ${by} | ${score} points`}
         actAsExpander={true}
         style={{display: "flex", alignItems:"center"}}
       />
+      <CardActions expandable={true} style={{float: "right", display: "flex", alignItems: "center"}}>
+        <IconButton tooltip={`${kids.length} comment`}>
+          <Comment />
+        </IconButton>
+        <IconButton href={url} tooltip="Open link">
+          <Link />
+        </IconButton>
+      </CardActions>
     </Card>
   }
 }
 
-export default Item;
+export default Item
