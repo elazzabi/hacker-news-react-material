74c6db13da92d81ec3af2f155fab4747c190b242
Ahmed <elazzabi.ahmed@gmail.com>
2017-03-01 23:19:23 +0000

    Remove comments

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import {Card, CardHeader, CardActions} from 'material-ui/Card'
-//import Avatar from 'material-ui/Avatar'
-//import { pink400 } from 'material-ui/styles/colors'
 import IconButton from 'material-ui/IconButton'
 import Link from 'material-ui/svg-icons/action/open-in-new'
 import Comment from 'material-ui/svg-icons/communication/comment'
@@ -12,8 +10,6 @@ class Item extends Component {
 
     return <Card style={{marginTop: "16px"}}>
       <CardHeader
-        // Commented due to stretching problems in Avatar
-        //avatar={<Avatar backgroundColor={pink400}>{title[0]}</Avatar>}
         title={title}
         subtitle={`by ${by} | ${score} points`}
         actAsExpander={true}
