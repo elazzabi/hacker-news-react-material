758e089c6391dc6f79ea393b2688cb3e150768a3
Ahmed <elazzabi.ahmed@gmail.com>
2017-03-01 22:43:07 +0000

    Fix tests and adding enzyme package

--- src/ItemsList.test.js
+++ src/ItemsList.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ItemsList from './ItemsList';
+import { shallow } from 'enzyme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin()
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ItemsList />, div);
+  ReactDOM.render(<MuiThemeProvider><ItemsList /></MuiThemeProvider>, div);
 });
--- src/Loading.test.js
+++ src/Loading.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Loading from './Loading';
+import { shallow } from 'enzyme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Loading />, div);
+  ReactDOM.render(<MuiThemeProvider><Loading /></MuiThemeProvider>, div);
 });
